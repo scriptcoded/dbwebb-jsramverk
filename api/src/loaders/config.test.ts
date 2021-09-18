b40ec7003eae78cdb7139b2af5474a831e3e8de2
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2021-09-18 22:48:31 +0200

    text: config

---
+++ api/src/loaders/config.test.ts
@@ -0,0 +1,33 @@
+import Container from 'typedi'
+
+import { loadConfig, CONFIG_TOKEN } from './config'
+
+// Make sure we restore process.env before each test
+let origEnv: NodeJS.ProcessEnv
+
+beforeAll(() => {
+  origEnv = { ...process.env }
+})
+
+beforeEach(() => {
+  process.env = { ...origEnv }
+  Container.reset()
+})
+
+test('should load config', () => {
+  process.env.PORT = '4000'
+  process.env.APP_KEYS = 'x'
+  process.env.DATABASE_URL = 'x'
+
+  loadConfig()
+
+  expect(typeof Container.get(CONFIG_TOKEN)).toBe('object')
+
+  expect(Container.get(CONFIG_TOKEN)).toMatchObject({
+    port: '4000'
+  })
+})
+
+test('should throw for missing environment variables', () => {
+  expect(() => loadConfig()).toThrow()
+})
